a63a945241b392fcda892d9e5f44945a670dec67
paNikitin <nikitin.palik@gmail.com>
2022-10-14 15:41:09 +0300

    v0.1

---
+++ backend/__backend__.py
@@ -0,0 +1,168 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import numpy as np
+from bs4 import BeautifulSoup
+import urllib.request
+from captcha_solver import CaptchaSolver
+
+options = webdriver.ChromeOptions()
+options.add_argument("--window-size=1024,768")
+options.add_argument("--headless")
+
+with open('driver_path.txt', 'r', encoding='utf-8') as file:
+    path = file.readline()
+    print(path)
+    file.close()
+driver = webdriver.Chrome(
+    executable_path=path,
+    options=options
+)
+
+driver = webdriver.Chrome(
+    executable_path=path,
+    options=options
+)
+url = "https://vk.com/"
+
+
+def elementInteraction(input):
+    ActionChains(driver) \
+        .move_to_element_with_offset(input, 8, 0) \
+        .click() \
+        .perform()
+
+
+def sendKeys(element, input):
+    element.clear()
+    element.send_keys(input)
+
+
+def getToMainPage(login, password, key):
+    driver.implicitly_wait(3)
+    driver.get(url=url)
+    time.sleep(2)
+    driver.save_screenshot("1.png")
+    email_input = driver.find_element(By.XPATH,
+                                      '//*[@id="index_email"]')
+    elementInteraction(email_input)
+    sendKeys(email_input, login)
+    time.sleep(1)
+    login_button = driver.find_element(By.XPATH,
+                                       '//*[@id="index_login"]/div/form/button[1]')
+    elementInteraction(login_button)
+    time.sleep(2)
+    if check_exists_by_xpath('//*[@id="root"]/div/div/div/div[2]/div/div/form/img'):
+        captchaSolve(key)
+        time.sleep(1)
+    password_input = driver.find_element(By.XPATH,
+                                         '//*[@id="root"]/div/div/div/div/div[2]/div/div/form/div[1]/div[3]/div[1]/div/input')
+    elementInteraction(password_input)
+    sendKeys(password_input, password)
+    time.sleep(1)
+    login_button = driver.find_element(By.XPATH,
+                                       '//*[@id="root"]/div/div/div/div/div[2]/div/div/form/div[2]/button')
+    elementInteraction(login_button)
+    time.sleep(2)
+    driver.save_screenshot("2.png")
+
+
+def check_exists_by_xpath(xpath):
+    try:
+        driver.find_element(By.XPATH, xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
+
+def scrapeElements(page):
+    doc = BeautifulSoup(page, 'html.parser')
+    details = doc.find_all('div', {'class': 'ProfileModalMiniInfoCell'})
+    output = ""
+    for param in details:
+        output += param.text
+        output += " | "
+    return output
+
+
+def singleHumanHandler(human_id):
+    driver.implicitly_wait(4)
+    driver.get(url=f'https://vk.com/id{human_id}')
+    time.sleep(2)
+    output = ""
+    if check_exists_by_xpath('//*[@id="owner_page_name"]'):
+        output = driver.find_element(By.XPATH, '//*[@id="owner_page_name"]').text
+        output += " | "
+        if check_exists_by_xpath(
+                '//*[@id="profile_redesigned"]/div/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div[2]/div/div[3]/div[2]'):
+            details = driver.find_element(By.XPATH,
+                                          '//*[@id="profile_redesigned"]/div/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div[2]/div/div[3]/div[2]')
+            elementInteraction(details)
+            time.sleep(3)
+            output += scrapeElements(driver.page_source)
+            output = "|" + str(human_id) + "| " + output
+        elif check_exists_by_xpath('//*[@id="profile_redesigned"]/div/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div[2]/div/div'):
+            details = driver.find_element(By.XPATH,
+                                          '//*[@id="profile_redesigned"]/div/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div[2]/div/div')
+            elementInteraction(details)
+            time.sleep(3)
+            output += scrapeElements(driver.page_source)
+            output = str(human_id) + "| " + output
+        else:
+            if check_exists_by_xpath(
+                    '//*[@id="profile_redesigned"]/div/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div[2]/div/div[3]/div[2]'):
+                details = driver.find_element(By.XPATH,
+                                              '//*[@id="profile_redesigned"]/div/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div[3]/div[2]')
+                elementInteraction(details)
+                time.sleep(3)
+                output += scrapeElements(driver.page_source)
+                output = "|" + str(human_id) + "| " + output
+            else:
+                output = "....| " + str(human_id) + " | " + output + "Профиль заблокирован."
+    else:
+        output = "???" + str(human_id) + "|" + "Профиль не существует."
+    return output
+
+
+def readIds(path):
+    ids = []
+    with open(path, 'r', encoding='utf-8') as file:
+        [ids.append(line) for line in file.readlines()]
+        file.close()
+    output = np.asarray(ids)
+    return output
+
+
+def writeOutput(array):
+    with open('output.txt', 'w', encoding='utf-8') as file:
+        for single_human in array:
+            file.writelines(single_human + "\n")
+    file.close()
+
+
+def captchaSolve(key):
+    img = driver.find_element(By.XPATH,
+                              '//*[@id="root"]/div/div/div/div[2]/div/div/form/img')
+    src = img.get_attribute('src')
+    urllib.request.urlretrieve(src, 'captcha.png')
+    time.sleep(1)
+    driver.save_screenshot("3.png")
+    solver = CaptchaSolver('rucaptcha', api_key=key)
+    raw_data = open('captcha.png', 'rb').read()
+    answer = solver.solve_captcha(raw_data)
+    captcha_input = driver.find_element(By.XPATH,
+                                        '//*[@id="root"]/div/div/div/div[2]/div/div/form/div[1]/div/div/input')
+    sendKeys(captcha_input, str(answer))
+    captcha_submit = driver.find_element(By.XPATH,
+                                         '//*[@id="root"]/div/div/div/div[2]/div/div/form/div[2]/button')
+
+    elementInteraction(captcha_submit)
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,138 @@
+import PySimpleGUI as sg
+import os.path
+from backend import __backend__ as go
+
+left_pane = [
+    [
+        sg.Text("Login"),
+    ],
+    [
+        sg.Text("Password"),
+    ],
+    [
+        sg.Text("Captcha solver API key"),
+    ],
+]
+right_pane = [
+    [
+        sg.Input(size=(25,1), enable_events=True, key="-LOGIN-", right_click_menu=[[''], ['Paste Login']]),
+    ],
+    [
+        sg.Input(size=(25,1), enable_events=True, key="-PASSWORD-", right_click_menu=[[''], ['Paste Password']], password_char='*'),
+    ],
+    [
+        sg.Input(size=(25,1), enable_events=True, key="-API-", right_click_menu=[[''], ['Paste API']]),
+    ],
+]
+left_layout = [
+    [
+        sg.Column(left_pane),
+        sg.VSeparator(),
+        sg.Column(right_pane)
+    ]
+]
+file_list_column = [
+    [
+        sg.Text("User IDs Folder"),
+        sg.In(size=(25,1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse(),
+    ],
+    [
+        sg.Listbox(values=[], enable_events=True, size=(40,5), key="-FILE LIST-")
+    ],
+    [
+      sg.Col(left_layout, vertical_alignment='center')
+    ],
+    [
+        sg.Button("Scrape", key="-SCRAPE-"),
+    ],
+]
+preview_column = [
+    [sg.Text("Progress:", s=(20,2))],
+    [sg.ProgressBar(100, orientation='h', s=(20,5), k='-PBAR-')]
+]
+layout = [
+    [
+        sg.Column(file_list_column),
+        sg.VSeparator(),
+        sg.Column(preview_column),
+    ]
+]
+window = sg.Window("snParser", layout, element_justification='c', font='Helvetica')
+# глобальные переменные
+filename = ""
+login = ""
+password = ""
+api = ""
+#login = 'nikitin.palik9@gmail.com'
+#password = 'Aboba228'
+#api_key='577183d0989f990556aff3fc409e21f8'
+while True:
+    event, values = window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
+    if event == "-FOLDER-":
+        folder = values["-FOLDER-"]
+        try:
+            file_list = os.listdir(folder)
+        except:
+            file_list = []
+        fnames = [
+            f
+            for f in file_list
+            if os.path.isfile(os.path.join(folder, f))
+            and f.lower().endswith((".txt"))
+        ]
+        window["-FILE LIST-"].update(fnames)
+    if event == "-FILE LIST-":
+        try:
+            filename = os.path.join(
+                values["-FOLDER-"],values["-FILE LIST-"][0]
+            )
+        except:
+            pass
+    if event == "-LOGIN-":
+        login = values["-LOGIN-"]
+    if event == "Paste Login":
+        window["-LOGIN-"].update(sg.clipboard_get())
+    if event == "-PASSWORD-":
+        password = values["-PASSWORD-"]
+    if event == "Paste Password":
+        window["-PASSWORD-"].update(sg.clipboard_get())
+    if event == "-API-":
+        api = values["-API-"]
+    if event == "Paste API":
+        window["-API-"].update(sg.clipboard_get())
+    if event == "-SCRAPE-":
+        args = ""
+        if not filename:
+            args += "Путь не указан. "
+        if not login:
+            args += "Логин не указан. "
+        if not password:
+            args += "Пароль не указан. "
+        if not api:
+            args += "АПИ-ключ не указан. "
+        if args:
+            sg.Popup(args, keep_on_top=True, title='Ошибка')
+        else:
+            try:
+
+                IDs = go.readIds(filename)
+                count = go.np.count_nonzero(IDs)
+                output_array = []
+                go.getToMainPage(login, password, api)
+                counter = 1
+                for single_id in go.np.nditer(IDs):
+                    output_array.append(go.singleHumanHandler(single_id))
+                    window["-PBAR-"].update((counter/count)*100)
+                    counter += 1
+                go.writeOutput(output_array)
+                go.driver.save_screenshot("4.png")
+            except Exception as ex:
+                print(ex)
+            finally:
+                go.driver.close()
+                go.driver.quit()
+
+window.close()
